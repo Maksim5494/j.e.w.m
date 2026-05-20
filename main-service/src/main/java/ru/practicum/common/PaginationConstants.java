950cff24fe2f069aa3cd97a07eb128525553737e
Макс <maksim5494@yandex.ru>
2026-05-20 20:45:08 +0500

    9

--- main-service/src/main/java/ru/practicum/common/PaginationConstants.java
+++ main-service/src/main/java/ru/practicum/common/PaginationConstants.java
@@ -3,8 +3,6 @@ package ru.practicum.common;
 public final class PaginationConstants {
 
     public static final int FIRST_PAGE_INDEX = 0;
-    public static final int SECOND_PAGE_INDEX = 1;
-    public static final int THIRD_PAGE_INDEX = 2;
 
     private PaginationConstants() {
     }
